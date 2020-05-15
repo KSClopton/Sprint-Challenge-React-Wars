5e5b75dae82a7f30c5c54a63de8c1dc8bf7b16b4
KSClopton <kyle.clopton@outlook.com>
2020-05-15 14:56:44 -0400

    finished mvp

--- starwars/src/components/CardMaker.jsx
+++ starwars/src/components/CardMaker.jsx
@@ -1,13 +1,49 @@
 import React from "react";
-import styled from "styled-components";
+import styled from "styled-components"
 
 const CardMaker = ({name}) => {
     return (
-        <div>
+        <StyleCards>
             <h2>{name.name}</h2>
-        </div>
+            <Attributes>
+                <p>Skin Color: {name.skin_color}</p>
+                <p>Height: {name.height}cm</p>
+                <p>Gender: {name.gender}</p>
+                <p>Mass: {name.mass}kg</p>
+            </Attributes>
+        </StyleCards>
+        
     )
 }
 
-const styleCards = styled.div
+const StyleCards = styled.div`
+    display: flex;
+    justify-content: space-between;
+    color: #A0522D;
+    text-shadow: 1px 1px 5px #fff;
+    border: solid grey 1px;
+    padding: 1px;
+    h2{
+        border: solid grey 2px;
+        margin: auto;
+        padding: 0 5% 0 0;
+        
+        text-shadow: 1px 1px 5px #fff;
+    }
+
+`
+const Attributes = styled.div`
+    display:flex;
+    flex-wrap: wrap;
+    width: 45%;
+    border-left: solid #F4A460 2px;
+    padding-left: 5%;
+
+    p{
+        width: 45%;
+    }
+
+
+
+`
 export default CardMaker;
